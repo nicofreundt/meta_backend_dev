1c1ea1d27b753c83bd9866ab5ce7781837297ca3
Nico Freundt <nico.freundt@protonmail.com>
2023-07-24 20:51:29 +0200

    features: week two

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(default=0)
+    booking_date = models.DateTimeField()
+
+    class Meta:
+        verbose_name = 'Booking'
+        verbose_name_plural = 'Bookings'
+
+    def __str__(self):
+        return self.name
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'Menu Item'
+        verbose_name_plural = 'Menu Items'
+
+    def __str__(self):
+        return self.title
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets, permissions
+from rest_framework.decorators import api_view
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+# Create your views here. 
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated] 
